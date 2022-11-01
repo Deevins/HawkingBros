92c17704a2ad245a7fe207f33cbad3efdcefb630
deevins <daker255@bk.ru>
2022-11-02 00:33:41 +0300

    initial commit ht 2.2

--- scripts.js
+++ scripts.js
@@ -1,75 +1,80 @@
+// input validation
 document.addEventListener('DOMContentLoaded', () => {
-// github info
+const errorContainer = document.querySelector(".error-container")
 const loadBtn = document.querySelector(".js-load")
-const resultsContainer = document.querySelector(".js-results")
-const searchInput = document.querySelector(".js-input")
-
+const userInput = document.querySelector(".input-name")
+let text;
 
 loadBtn.addEventListener("click", (event) => {
   event.preventDefault()
-  const searchValue = searchInput.value.trim().toLowerCase()
-  
-  if(!searchValue) return
+  const inputValue = userInput.value
 
-  fetch(`https://api.github.com/usedrs/${searchValue}`)
-    .then(response => response.json()
-    .then(data => (
-            resultsContainer.innerHTML = `
-              <div class="response-container">
-                <img src="${data?.avatar_url}">
-                <p> Имя: <span>${data?.name}</span><p>
-                <p> О себе: <span>${data?.bio}</span><p>
-                <p> Кол-во репозиториев: <span>${data?.public_repos}</span><p>
-              </div>`
-    ))
-    .catch(error => {
-            console.error(error)
-            resultsContainer.innerHTML = `
-             <div class="response-container">
-                Пользователь не найден.
-              </div>`
+  if(isNaN(inputValue) || !inputValue) {
+    errorContainer.classList.add('input-error-container')
+    userInput.classList.add('input-error')
+    errorContainer.innerHTML = `<p>Введите, пожалуйста, число</p>`
+    console.log(1)
+    return
+
+  } 
+  if(inputValue < 5 || inputValue > 10){
+    errorContainer.classList.add('input-error-container')
+    userInput.classList.add('input-error')
+    errorContainer.innerHTML = `<p>Введите, пожалуйста, корректное число</p>`
+    console.log(2)
+  }
+  else{
+    userInput.classList.remove('input-error')
+    errorContainer.classList.remove('input-error-container')
+    errorContainer.innerHTML = ``
+    console.log(3)
+
+  }
     })
- )
-})
 
-
-// users fetching
-const fetchButton = document.querySelector('.js-fetch')
-const fetchResultsContainer = document.querySelector(".js-fetch-results")
-const mockDataUrl = 'https://jsonplaceholder.typicode.com/users'
-
-
-const requestMockData = (url = mockDataUrl) => axios.get(url)
-
-
-const userBlock = (data) => (
-        `<div class="results-list-item">
-              <p> Имя: <span>${data?.name}</span><p>
-              <p> Никнейм: <span>${data?.username}</span><p> 
-              <p> Сайт: <span>${data?.website}</span><p>
-              <p> Компания: <span>${data?.company.name}</span><p>
-          </div>`
-
-)
-
-
-
-  fetchButton.addEventListener('click', event => {
-    event.preventDefault();
-    fetchResultsContainer.innerHTML = ``
-  
-    requestMockData()
-      .then(response => {
-        if (!response.data.length) 
-          fetchResultsContainer.innerHTML = userBlock(response.data) 
-       
- 
-       for (let i = 0; i < response.data.length; i++) 
-        fetchResultsContainer.innerHTML += userBlock(response.data[i])
-
-     })
 
   })
 
+
+  // lottery async/await
+
+  // function lottery() {
+  //   console.log("Вы начали игру");
+  //   let promise = new Promise(function (resolve, reject) {
+  //     setTimeout(function () {
+  //       Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
+  //     }, 1000);
+  //   });
+  //   return promise;
+  // }
   
-})
+  // lottery()
+  //   .then(() => console.log("Вы выиграли"))
+  //   .then(() => console.log("Вам заплатили"))
+  //   .catch(() => console.log("Вы проиграли"))
+  //   .finally(() => console.log("Игра закончена"));
+
+  function lottery() {
+    let happyNum
+
+    try {
+
+    } catch(er) {
+
+    }
+    console.log("Вы начали игру");
+    let promise = new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
+      }, 1000);
+    });
+    return promise;
+  }
+  
+  lottery()
+    .then(() => console.log("Вы выиграли"))
+    .then(() => console.log("Вам заплатили"))
+    .catch(() => console.log("Вы проиграли"))
+    .finally(() => console.log("Игра закончена"));
+  
+  
